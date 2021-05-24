d6ef3b15de946fa498f5448790c37b10429774ba
nandiniganeshs1 <>
2021-05-24 15:16:45 +0000

    printf statements

--- Program_1.c
+++ Program_1.c
@@ -1,1 +1,13 @@
 //Write a C Program to Display your Name
+
+#include<stdio.h>
+void main()
+{
+  
+  printf("My name is nandini.g\n");
+  printf("Computer science and engineering\n");
+  printf("vidyavardaka college of engineering\n");
+
+  
+}
+
